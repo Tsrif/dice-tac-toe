0668d567513a2211333bae09f553ede0201d3e54
Ricky <rickyrivera00@yahoo.com>
2022-12-05 14:32:41 -0500

    actual init

---
+++ src/components/game-board-box.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">eeep</div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
+</script>
