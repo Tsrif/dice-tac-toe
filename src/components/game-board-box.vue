a9eb822f74d695bd4c82698a8403149f0981abc0
Ricky <rickyrivera00@yahoo.com>
2022-12-16 12:30:44 -0500

    add easy dnd package

--- src/components/game-board-box.vue
+++ src/components/game-board-box.vue
@@ -1,25 +1,42 @@
 <template>
   <div
     class="border border-white w-auto p-10 drop-zone"
-    @drop="onDrop($event, 1)"
+    @drop="onDrop($event)"
     @dragover.prevent
     @dragenter.prevent
   >
     <!-- where the game piece goes 
       I'm thinking I could always have a game piece in here, and then when you drop a new game piece in here, it'll just take all the data from the new one and apply it to the one that's in here already???
     -->
-    <slot></slot>
+    <!-- <slot></slot> -->
+    <!-- <gamePiece pieceColor="blue" size="2"></gamePiece> -->
   </div>
 </template>
 
-<script lang="ts">
-export default {
-  methods: {
-    onDrop(evt, list) {
-      console.log(evt);
-      // const item = this.items.find((item) => item.id == itemID);
-      // item.list = list;
-    },
-  },
-};
+<script setup lang="ts">
+import { computed } from "vue";
+import gamePiece from "./game-piece.vue";
+
+function onDrop(evt) {
+  console.log("----ON DROP----");
+  console.log(evt);
+  //console.log(getDataFromSlot);
+  // const item = this.items.find((item) => item.id == itemID);
+  // item.list = list;
+}
+
+const getDataFromSlot = computed(() => {
+  return this.$slot.data;
+});
+
+// export default {
+//   methods: {
+//     onDrop(evt) {
+//       console.log("----ON DROP----");
+//       console.log(evt);
+//       // const item = this.items.find((item) => item.id == itemID);
+//       // item.list = list;
+//     },
+//   },
+// };
 </script>
--- src/components/game-board.vue
+++ src/components/game-board.vue
@@ -1,6 +1,11 @@
-<template>
+<!-- <template>
   <div>
-    <gamePiece pieceColor="red" size="2" id="1"></gamePiece>
+    <gamePiece
+      pieceColor="red"
+      size="2"
+      id="1"
+      @sendData="captureGamePieceData"
+    ></gamePiece>
     <gamePiece pieceColor="red" size="2"></gamePiece>
     <gamePiece pieceColor="red" size="2"></gamePiece>
     <gamePiece pieceColor="blue" size="2"></gamePiece>
@@ -9,7 +14,7 @@
     <div class="grid grid-cols-3 gap-3">
       <div>
         <GameBoardBox>
-          <!-- <gamePiece pieceColor="blue" size="2"></gamePiece> -->
+          <gamePiece pieceColor="blue" size="2"></gamePiece>
         </GameBoardBox>
       </div>
       <div><GameBoardBox></GameBoardBox></div>
@@ -24,15 +29,161 @@
       <div><GameBoardBox></GameBoardBox></div>
     </div>
   </div>
+</template> -->
+
+<template>
+  <div id="app">
+    <transition-group name="list" tag="div">
+      <drag
+        v-for="piece in allPieces"
+        :key="piece.id"
+        class="drag"
+        :data="piece"
+        @cut="remove(piece)"
+      >
+        <gamePiece :pieceColor="piece.pieceColor" :size="piece.size">
+        </gamePiece>
+      </drag>
+    </transition-group>
+    <div class="group">
+      <drop class="copy" @drop="onCopyDrop">
+        <span v-for="(n, index) in copied" :key="index"
+          >Copied {{ n }};&nbsp;</span
+        >
+      </drop>
+      <drop class="cut" @drop="onCutDrop" mode="cut">
+        <span v-for="(piece, index) in cut" :key="index"
+          >Cut
+          <gamePiece :pieceColor="piece.pieceColor" :size="piece.size">
+          </gamePiece
+          >;&nbsp;</span
+        >
+      </drop>
+    </div>
+  </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import GameBoardBox from "./game-board-box.vue";
 import gamePiece from "./game-piece.vue";
-export default {
-  components: { GameBoardBox, gamePiece },
-  data() {
-    return {};
-  },
-};
+import { ref, computed } from "vue";
+import { Drag, Drop, DropMask } from "vue-easy-dnd";
+
+class GamePieceData {
+  constructor(
+    public id: number,
+    public position: string,
+    public pieceColor: string,
+    public size: string
+  ) {
+    this.id = id;
+    this.position = position;
+    this.pieceColor = pieceColor;
+    this.size = size;
+  }
+}
+
+const allPieces = ref([
+  new GamePieceData(1, "0", "red", "1"),
+  new GamePieceData(2, "0", "red", "1"),
+  new GamePieceData(3, "0", "red", "1"),
+]);
+const copied = ref([]);
+const cut = ref([]);
+
+function onCopyDrop(e) {
+  copied.value.push(e.data);
+}
+function onCutDrop(e) {
+  cut.value.push(e.data);
+}
+function remove(n) {
+  let index = allPieces.value.indexOf(n);
+  allPieces.value.splice(index, 1);
+}
 </script>
+
+<style>
+html,
+body {
+  height: 100%;
+  font-family: "Roboto";
+}
+
+.drag {
+  width: 60px;
+  height: 60px;
+  background-color: rgb(220, 220, 255);
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  margin: 10px 10px 0 10px;
+  font-size: 20px;
+  transition: all 0.5s;
+}
+
+.group {
+  display: flex;
+}
+
+.copy {
+  margin: 20px 10px;
+  border: 1px solid black;
+  height: 100px;
+  display: inline-block;
+  position: relative;
+  flex: 1;
+}
+
+.cut {
+  margin: 20px 10px;
+  border: 1px solid black;
+  height: 100px;
+  display: inline-block;
+  position: relative;
+  flex: 1;
+}
+
+.copy::before {
+  content: "COPY";
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  color: rgba(0, 0, 0, 0.4);
+  font-size: 25px;
+  font-weight: bold;
+}
+
+.cut::before {
+  content: "CUT";
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  color: rgba(0, 0, 0, 0.4);
+  font-size: 25px;
+  font-weight: bold;
+}
+
+.drop-allowed {
+  background-color: rgba(0, 255, 0, 0.2);
+}
+
+.drop-forbidden {
+  background-color: rgba(255, 0, 0, 0.2);
+}
+
+.drop-in {
+  box-shadow: 0 0 5px rgba(0, 0, 255, 0.4);
+}
+
+.list-enter,
+.list-leave-to {
+  opacity: 0;
+}
+
+.list-leave-active {
+  position: absolute;
+}
+</style>
