109682893433d19a920fafada02abf7f659b2920
Ricky <rickyrivera00@yahoo.com>
2022-12-16 18:19:38 -0500

    still gross, but works better and kinda have basic placing mechanic in

--- src/components/game-board.vue
+++ src/components/game-board.vue
@@ -1,12 +1,25 @@
 <template>
   <div>
-    <transition-group name="list" tag="div">
+    <transition-group name="list" tag="div" class="grid grid-cols-3 gap-3">
       <drag
-        v-for="piece in allPieces"
+        v-for="piece in player1Pieces"
         :key="piece.id"
         class="drag"
         :data="piece"
-        @cut="remove(piece)"
+        @cut="removep1(piece)"
+      >
+        <gamePiece :pieceColor="piece.pieceColor" :size="piece.size">
+        </gamePiece>
+      </drag>
+    </transition-group>
+
+    <transition-group name="list" tag="div" class="grid grid-cols-3 gap-3">
+      <drag
+        v-for="piece in player2Pieces"
+        :key="piece.id"
+        class="drag"
+        :data="piece"
+        @cut="removep2(piece)"
       >
         <gamePiece :pieceColor="piece.pieceColor" :size="piece.size">
         </gamePiece>
@@ -17,9 +30,19 @@
         class="border border-white p-10"
         @drop="onCutDrop1($event)"
         mode="cut"
+        :accepts-data="
+          (p) => {
+            if (box1Data == undefined) {
+              return true;
+            } else if (p.size > box1Data.size) {
+              return true;
+            }
+            return false;
+          }
+        "
       >
-        <span v-for="(piece, index) in box1Data" :key="index">
-          <gamePiece :pieceColor="piece.pieceColor" :size="piece.size">
+        <span v-if="box1Data">
+          <gamePiece :pieceColor="box1Data.pieceColor" :size="box1Data.size">
           </gamePiece>
         </span>
       </drop>
@@ -28,9 +51,19 @@
         class="border border-white p-10"
         @drop="onCutDrop2($event)"
         mode="cut"
+        :accepts-data="
+          (p) => {
+            if (box2Data == undefined) {
+              return true;
+            } else if (p.size > box2Data.size) {
+              return true;
+            }
+            return false;
+          }
+        "
       >
-        <span v-for="(piece, index) in box2Data" :key="index">
-          <gamePiece :pieceColor="piece.pieceColor" :size="piece.size">
+        <span v-if="box2Data">
+          <gamePiece :pieceColor="box2Data.pieceColor" :size="box2Data.size">
           </gamePiece
         ></span>
       </drop>
@@ -39,9 +72,19 @@
         class="border border-white p-10"
         @drop="onCutDrop3($event)"
         mode="cut"
+        :accepts-data="
+          (p) => {
+            if (box3Data == undefined) {
+              return true;
+            } else if (p.size > box3Data.size) {
+              return true;
+            }
+            return false;
+          }
+        "
       >
-        <span v-for="(piece, index) in box3Data" :key="index">
-          <gamePiece :pieceColor="piece.pieceColor" :size="piece.size">
+        <span v-if="box3Data">
+          <gamePiece :pieceColor="box3Data.pieceColor" :size="box3Data.size">
           </gamePiece
         ></span>
       </drop>
@@ -50,9 +93,19 @@
         class="border border-white p-10"
         @drop="onCutDrop4($event)"
         mode="cut"
+        :accepts-data="
+          (p) => {
+            if (box4Data == undefined) {
+              return true;
+            } else if (p.size > box4Data.size) {
+              return true;
+            }
+            return false;
+          }
+        "
       >
-        <span v-for="(piece, index) in box4Data" :key="index">
-          <gamePiece :pieceColor="piece.pieceColor" :size="piece.size">
+        <span v-if="box4Data">
+          <gamePiece :pieceColor="box4Data.pieceColor" :size="box4Data.size">
           </gamePiece
         ></span>
       </drop>
@@ -61,9 +114,19 @@
         class="border border-white p-10"
         @drop="onCutDrop5($event)"
         mode="cut"
+        :accepts-data="
+          (p) => {
+            if (box5Data == undefined) {
+              return true;
+            } else if (p.size > box5Data.size) {
+              return true;
+            }
+            return false;
+          }
+        "
       >
-        <span v-for="(piece, index) in box5Data" :key="index">
-          <gamePiece :pieceColor="piece.pieceColor" :size="piece.size">
+        <span v-if="box5Data">
+          <gamePiece :pieceColor="box5Data.pieceColor" :size="box5Data.size">
           </gamePiece
         ></span>
       </drop>
@@ -72,9 +135,19 @@
         class="border border-white p-10"
         @drop="onCutDrop6($event)"
         mode="cut"
+        :accepts-data="
+          (p) => {
+            if (box6Data == undefined) {
+              return true;
+            } else if (p.size > box6Data.size) {
+              return true;
+            }
+            return false;
+          }
+        "
       >
-        <span v-for="(piece, index) in box6Data" :key="index">
-          <gamePiece :pieceColor="piece.pieceColor" :size="piece.size">
+        <span v-if="box6Data">
+          <gamePiece :pieceColor="box6Data.pieceColor" :size="box6Data.size">
           </gamePiece
         ></span>
       </drop>
@@ -83,9 +156,19 @@
         class="border border-white p-10"
         @drop="onCutDrop7($event)"
         mode="cut"
+        :accepts-data="
+          (p) => {
+            if (box7Data == undefined) {
+              return true;
+            } else if (p.size > box7Data.size) {
+              return true;
+            }
+            return false;
+          }
+        "
       >
-        <span v-for="(piece, index) in box7Data" :key="index">
-          <gamePiece :pieceColor="piece.pieceColor" :size="piece.size">
+        <span v-if="box7Data">
+          <gamePiece :pieceColor="box7Data.pieceColor" :size="box7Data.size">
           </gamePiece
         ></span>
       </drop>
@@ -94,9 +177,19 @@
         class="border border-white p-10"
         @drop="onCutDrop8($event)"
         mode="cut"
+        :accepts-data="
+          (p) => {
+            if (box8Data == undefined) {
+              return true;
+            } else if (p.size > box8Data.size) {
+              return true;
+            }
+            return false;
+          }
+        "
       >
-        <span v-for="(piece, index) in box8Data" :key="index">
-          <gamePiece :pieceColor="piece.pieceColor" :size="piece.size">
+        <span v-if="box8Data">
+          <gamePiece :pieceColor="box8Data.pieceColor" :size="box8Data.size">
           </gamePiece
         ></span>
       </drop>
@@ -105,9 +198,19 @@
         class="border border-white p-10"
         @drop="onCutDrop9($event)"
         mode="cut"
+        :accepts-data="
+          (p) => {
+            if (box9Data == undefined) {
+              return true;
+            } else if (p.size > box9Data.size) {
+              return true;
+            }
+            return false;
+          }
+        "
       >
-        <span v-for="(piece, index) in box9Data" :key="index">
-          <gamePiece :pieceColor="piece.pieceColor" :size="piece.size">
+        <span v-if="box9Data">
+          <gamePiece :pieceColor="box9Data.pieceColor" :size="box9Data.size">
           </gamePiece
         ></span>
       </drop>
@@ -118,14 +221,14 @@
 <script setup lang="ts">
 import GameBoardBox from "./game-board-box.vue";
 import gamePiece from "./game-piece.vue";
-import { ref, computed } from "vue";
+import { ref, computed, toRaw } from "vue";
 import { Drag, Drop, DropMask } from "vue-easy-dnd";
 
 class GamePieceData {
   constructor(
     public id: number,
     public pieceColor: string,
-    public size: string
+    public size: number
   ) {
     this.id = id;
     this.pieceColor = pieceColor;
@@ -134,56 +237,87 @@ class GamePieceData {
 }
 
 const allPieces = ref([
-  new GamePieceData(1, "red", "1"),
-  new GamePieceData(2, "blue", "1"),
-  new GamePieceData(3, "red", "1"),
+  new GamePieceData(1, "red", 1),
+  new GamePieceData(2, "blue", 1),
+  new GamePieceData(3, "red", 1),
 ]);
 
-const box1Data = ref([]);
-const box2Data = ref([]);
-const box3Data = ref([]);
-const box4Data = ref([]);
-const box5Data = ref([]);
-const box6Data = ref([]);
-const box7Data = ref([]);
-const box8Data = ref([]);
-const box9Data = ref([]);
+const player1Pieces = ref([
+  new GamePieceData(1, "red", 1),
+  new GamePieceData(2, "red", 1),
+  new GamePieceData(3, "red", 2),
+  new GamePieceData(4, "red", 2),
+  new GamePieceData(5, "red", 3),
+  new GamePieceData(6, "red", 3),
+]);
+
+const player2Pieces = ref([
+  new GamePieceData(1, "blue", 1),
+  new GamePieceData(2, "blue", 1),
+  new GamePieceData(3, "blue", 2),
+  new GamePieceData(4, "blue", 2),
+  new GamePieceData(5, "blue", 3),
+  new GamePieceData(6, "blue", 3),
+]);
+
+const box1Data = ref();
+const box2Data = ref();
+const box3Data = ref();
+const box4Data = ref();
+const box5Data = ref();
+const box6Data = ref();
+const box7Data = ref();
+const box8Data = ref();
+const box9Data = ref();
 
 //this is terrible
 function onCutDrop1(e: Event) {
-  box1Data.value.splice(0, 1, e.data);
-  //box1Data.value.cut(e.data);
+  box1Data.value = e.data;
 }
 function onCutDrop2(e: Event) {
-  box2Data.value.splice(0, 1, e.data);
+  box2Data.value = e.data;
 }
 function onCutDrop3(e: Event) {
-  box3Data.value.splice(0, 1, e.data);
+  box3Data.value = e.data;
 }
 
 function onCutDrop4(e: Event) {
-  box4Data.value.splice(0, 1, e.data);
+  box4Data.value = e.data;
 }
 function onCutDrop5(e: Event) {
-  box5Data.value.splice(0, 1, e.data);
+  box5Data.value = e.data;
 }
 function onCutDrop6(e: Event) {
-  box6Data.value.splice(0, 1, e.data);
+  box6Data.value = e.data;
 }
 
 function onCutDrop7(e: Event) {
-  box7Data.value.splice(0, 1, e.data);
+  box7Data.value = e.data;
 }
 function onCutDrop8(e: Event) {
-  box8Data.value.splice(0, 1, e.data);
+  box8Data.value = e.data;
 }
 function onCutDrop9(e: Event) {
-  box9Data.value.splice(0, 1, e.data);
+  box9Data.value = e.data;
 }
 
-function remove(n) {
-  let index = allPieces.value.indexOf(n);
-  allPieces.value.splice(index, 1);
+function checkCanPlace(piece1: GamePieceData, piece2: GamePieceData) {
+  console.log(toRaw(piece1));
+  if (piece1 === undefined || toRaw(piece2).size > piece1.size) {
+    return true;
+  }
+  return false;
+}
+
+//just going to comment out the remove logic for now because it'll remove stuff even if you aren't allowed to place a piece
+function removep1(n) {
+  // let index = player1Pieces.value.indexOf(n);
+  // player1Pieces.value.splice(index, 1);
+}
+
+function removep2(n) {
+  // let index = player2Pieces.value.indexOf(n);
+  // player2Pieces.value.splice(index, 1);
 }
 </script>
 
--- src/components/game-piece.vue
+++ src/components/game-piece.vue
@@ -1,7 +1,27 @@
 <template>
-  <div class="p-1">
-    <div class="inline-block" :style="{ 'background-color': pieceColor }">
-      {{ pieceColor }} : {{ size }}
+  <div class="p-3">
+    <div
+      v-if="size === 1"
+      class="h-5 w-5 rotate-45 transform origin-bottom-left"
+      :style="{ 'background-color': pieceColor }"
+    >
+      {{ size }}
+    </div>
+
+    <div
+      v-else-if="size === 2"
+      class="p-2 inline-block"
+      :style="{ 'background-color': pieceColor }"
+    >
+      {{ size }}
+    </div>
+
+    <div
+      v-else-if="size === 3"
+      class="p-2 rounded-full inline-block items-center justify-center"
+      :style="{ 'background-color': pieceColor }"
+    >
+      {{ size }}
     </div>
   </div>
 </template>
@@ -10,8 +30,7 @@
 import { ref } from "vue";
 
 const props = defineProps({
-  id: { type: String, default: "0", required: true },
   pieceColor: { type: String, default: "red", required: true },
-  size: { type: String, default: "1", required: true },
+  size: { type: Number, default: 1, required: true },
 });
 </script>
